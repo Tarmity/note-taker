44061e3260f779cc3a90222a573d7916f16fbae5
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-02 15:27:01 +1000

    add the dependencies

---
+++ sever.js
@@ -0,0 +1,23 @@
+// Dependencies ================================================
+
+const express = require("express");
+const path = require("path");
+const fs = require ("fs");
+
+// Set Up the Express App ======================================
+const app = express()
+const PORT= 8080; 
+
+
+// Html routes 
+
+
+
+//API  GET Routes
+
+
+// API Post Route
+
+
+// API Delete Route
+
